152214f8655d3934f4fd6bcbc2349291ac08e414
Your Name <u513509754@gmail.com>
2026-01-20 11:35:00 -0700

    soltion

--- src/types/Todo.ts
+++ src/types/Todo.ts
@@ -1,4 +1,13 @@
+import { JSX } from 'react/jsx-runtime';
+
 export interface Todo {
+  map(
+    arg0: (todo: {
+      id: React.Key;
+      title: string;
+      completed: string;
+    }) => JSX.Element,
+  ): import('react').ReactNode;
   id: number;
   title: string;
   completed: boolean;
